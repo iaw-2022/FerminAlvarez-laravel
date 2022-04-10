6f3bf6e4080b7186e7a98dc06e4e255e337b1d03
Fermin <feeralvarez013@gmail.com>
2022-04-10 17:46:02 -0300

    Implementación de datatables, responsive, filtros, paginación y orden

--- resources/views/bookshops/index.blade.php
+++ resources/views/bookshops/index.blade.php
@@ -1,28 +1,62 @@
 @extends('layouts.template')
+
+@section('css')
+    <!-- Boostrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- Boostrap Datatables -->
+    <link href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap5.min.css" rel="stylesheet">
+    <!-- Datatables responsive -->
+    <link href="https://cdn.datatables.net/responsive/2.2.9/css/responsive.bootstrap5.min.css" rel="stylesheet">
+    <!-- Google Icons -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="css/styles.css" rel="stylesheet">
+@endsection
+
 @section('title', 'Librerías')
 @section('content')
-<div class="table-container table-responsive-xxl d-flex aligns-items-center mt-3">
-    <table class="table align-middle mb-0 table-striped ">
-        <thead class="bg-light text-center">
-            <tr>
-                <th>ID</th>
-                <th>Nombre</th>
-                <th>Ciudad</th>
-                <th>Latitud</th>
-                <th>Longitud</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($bookshops as $bookshop)
-                <tr class="text-center">
-                    <td>{{$bookshop->id}}</td>
-                    <td class ="fw-bold mb-1">{{$bookshop->name}}</td>
-                    <td>{{$bookshop->city}}</td>
-                    <td>{{$bookshop->latitude}}</td>
-                    <td>{{$bookshop->longitude}}</td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
+    <div class="row">
+        <div class="aligns-items-center mt-3 col-lg-12">
+            <table class="table align-middle table-striped display nowrap" cellspacing="0" id="bookshops-table" width=100%>
+                <thead class="bg-light text-center">
+                    <tr>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th>Ciudad</th>
+                        <th>Latitud</th>
+                        <th>Longitud</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($bookshops as $bookshop)
+                        <tr class="text-center">
+                            <td>{{$bookshop->id}}</td>
+                            <td class ="fw-bold mb-1">{{$bookshop->name}}</td>
+                            <td>{{$bookshop->city}}</td>
+                            <td>{{$bookshop->latitude}}</td>
+                            <td>{{$bookshop->longitude}}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    @section('js')
+        <!-- Bootstrap -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <!-- JQuery datatables -->
+        <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+        <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
+        <!-- Datatables responsive datatables -->
+        <script src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>
+        <script src="https://cdn.datatables.net/responsive/2.2.9/js/responsive.bootstrap5.min.js"></script>
+
+        <script>$(document).ready(function() {
+            $('#bookshops-table').DataTable({
+                responsive:true
+            });});
+        </script>
+    @endsection
 @endsection
--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -5,22 +5,20 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet">
-
-    <link href="css/styles.css" rel="stylesheet">
+    @yield('css')
 
     <title>@yield('title')</title>
   </head>
   <body>
 
-    @include('header')
+    @include('partials.header')
 
     <div class="container">
         @yield('content')
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    @yield('js')
+
   </body>
+
 </html>
