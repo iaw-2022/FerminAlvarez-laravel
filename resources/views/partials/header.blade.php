42778f7eb1cdb695450cddd13209ef9de1ad11ab
Fermin <feeralvarez013@gmail.com>
2022-04-17 18:38:10 -0300

    Se guarda el estado del sidebar

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,18 +1,17 @@
 <header class="header" id="header">
     <div class="header_toggle"> <i class='bx bx-menu' id="header-toggle"></i> </div>
     <div class="nav-item dropdown">
-            <a class="dropdown-toggle text-dark" href="#" id="navbarDropdown" role="button"
-                data-bs-toggle="dropdown" aria-expanded="false">
-                {{ Auth::user()->name }}
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li>
-                    <a class="dropdown-item" href="javascript:;"
-                        onclick="document.getElementById('logout-form').submit();">
-                        {{ __('Cerrar sesión') }}
-                    </a>
-                </li>
-            </ul>
+        <a class="dropdown-toggle text-dark" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+            aria-expanded="false">
+            {{ Auth::user()->name }}
+        </a>
+        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li>
+                <a class="dropdown-item" href="javascript:;" onclick="document.getElementById('logout-form').submit();">
+                    {{ __('Cerrar sesión') }}
+                </a>
+            </li>
+        </ul>
     </div>
 </header>
 <div class="l-navbar" id="nav-bar">
@@ -39,11 +38,10 @@
                     <i class='bx bx-user-plus nav_icon'></i>
                     <span class="text-light">Suscriptores</span>
                 </a>
-
             </div>
         </div>
         <form id="logout-form" action="{{ route('logout') }}" method="POST">
-            <a href ="javascript:;" class="nav_link" onclick="document.getElementById('logout-form').submit();">
+            <a href="javascript:;" class="nav_link" onclick="document.getElementById('logout-form').submit();">
                 <i class='bx bx-log-out nav_icon'></i>
                 <span class="text-light">Cerrar sesión</span>
             </a>
@@ -54,7 +52,6 @@
 
 <script>
     document.addEventListener("DOMContentLoaded", function(event) {
-
         const showNavbar = (toggleId, navId, bodyId, headerId) => {
             const toggle = document.getElementById(toggleId),
                 nav = document.getElementById(navId),
@@ -63,11 +60,12 @@
 
             // Validate that all variables exist
             if (toggle && nav && bodypd && headerpd) {
-
-                nav.classList.toggle('show')
-                toggle.classList.toggle('bx-x')
-                bodypd.classList.toggle('body-pd')
-                headerpd.classList.toggle('body-pd')
+                if (getCookie("open") == "true"){
+                    nav.classList.toggle('show')
+                    toggle.classList.toggle('bx-x')
+                    bodypd.classList.toggle('body-pd')
+                    headerpd.classList.toggle('body-pd')
+                }
 
                 toggle.addEventListener('click', () => {
                     // show navbar
@@ -83,4 +81,30 @@
         }
         showNavbar('header-toggle', 'nav-bar', 'body-pd', 'header')
     });
+
+    document.getElementById("header-toggle").addEventListener("click",toggleCookie);
+
+    function toggleCookie(){
+        var openToggle = getCookie("open")
+        if (openToggle == "true")
+            document.cookie = "open = false"
+        else
+            document.cookie = "open = true"
+    }
+
+    function getCookie(cname) {
+        let name = cname + "=";
+        let decodedCookie = decodeURIComponent(document.cookie);
+        let ca = decodedCookie.split(';');
+        for(let i = 0; i <ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
 </script>
