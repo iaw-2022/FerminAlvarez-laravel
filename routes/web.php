8880c58c34d0a7ba36d155e13c0bb39a82d8e863
Fermin <feeralvarez013@gmail.com>
2022-04-23 11:21:32 -0300

    Middleware para nuevas rutas

--- routes/web.php
+++ routes/web.php
@@ -24,8 +24,8 @@ Route::resource('suscribers', 'App\Http\Controllers\SuscriberController')->middl
 
 Route::resource('authors', 'App\Http\Controllers\AuthorController')->middleware(['auth']);
 
-Route::get('/bookshop/{id}/book/{ISBN}', [BookshopController::class, 'showOnlinePrice']);
+Route::get('/bookshop/{id}/book/{ISBN}', [BookshopController::class, 'showOnlinePrice'])->middleware(['auth']);
 
-Route::put('/bookshop/{id}/book/{ISBN}/update', [BookshopController::class, 'updatePrice']);
+Route::put('/bookshop/{id}/book/{ISBN}/update', [BookshopController::class, 'updatePrice'])->middleware(['auth']);
 
 require __DIR__ . '/auth.php';
