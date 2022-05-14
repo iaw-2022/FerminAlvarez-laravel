0c9c093890bee96c43b204f08ad68153abd75952
Fermin <feeralvarez013@gmail.com>
2022-05-14 18:58:28 -0300

    Establece fecha inicial para relacion has del seeder

--- database/seeders/HasSeeder.php
+++ database/seeders/HasSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class HasSeeder extends Seeder
 {
@@ -19,37 +20,51 @@ class HasSeeder extends Seeder
             [
                 'price' => '6005',
                 'ISBN' => '9786073132411',
-                'Bookshop' => '4'
+                'Bookshop' => '4',
+                'created_at' =>  Carbon::create('2000', '01', '01'),
+                'updated_at' =>  Carbon::create('2000', '01', '01'),
             ],
             [
                 'price' => '4199',
                 'ISBN' => '9788466662321',
-                'Bookshop' => '3'
+                'Bookshop' => '3',
+                'created_at' =>  Carbon::create('2000', '01', '01'),
+                'updated_at' => Carbon::create('2000', '01', '01'),
             ],
             [
                 'price' => '4199',
                 'ISBN' => '9788466662321',
-                'Bookshop' => '2'
+                'Bookshop' => '2',
+                'created_at' =>  Carbon::create('2000', '01', '01'),
+                'updated_at' =>  Carbon::create('2000', '01', '01'),
             ],
             [
                 'price' => '3200',
                 'ISBN' => '9788499088990',
-                'Bookshop' => '4'
+                'Bookshop' => '4',
+                'created_at' =>  Carbon::create('2000', '01', '01'),
+                'updated_at' =>  Carbon::create('2000', '01', '01'),
             ],
             [
                 'price' => '3600',
                 'ISBN' => '9788466659840',
-                'Bookshop' => '4'
+                'Bookshop' => '4',
+                'created_at' =>  Carbon::create('2000', '01', '01'),
+                'updated_at' =>  Carbon::create('2000', '01', '01'),
             ],
             [
                 'price' => '4199',
                 'ISBN' => '9788466658843',
-                'Bookshop' => '4'
+                'Bookshop' => '4',
+                'created_at' =>  Carbon::create('2000', '01', '01'),
+                'updated_at' =>  Carbon::create('2000', '01', '01'),
             ],
             [
                 'price' => '4199',
                 'ISBN' => '9788466662321',
-                'Bookshop' => '4'
+                'Bookshop' => '4',
+                'created_at' =>  Carbon::create('2000', '01', '01'),
+                'updated_at' =>  Carbon::create('2000', '01', '01'),
             ]
         ];
         DB::table('has')->insert($data);
